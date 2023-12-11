8dc3b6bc58ef2ba18feca8b9a70e85191b9d470e
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-11 21:11:09 +0545

    Add movie routes to API

--- server/routes/routes.api.js
+++ server/routes/routes.api.js
@@ -1,6 +1,6 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const movieRouter = require("../modules/movies/movie.routes");
 
+router.use("/movies", movieRouter);
 
-
-
-module.exports = router
+module.exports = router;
