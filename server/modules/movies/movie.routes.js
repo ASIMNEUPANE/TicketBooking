0c7226ed26d4667144137d7a076217957ec2ba3b
ASIMNEUPANE <124865108+ASIMNEUPANE@users.noreply.github.com>
2023-12-12 21:40:27 +0545

    Refactor movie routes and add secureAPI middleware

--- server/modules/movies/movie.routes.js
+++ server/modules/movies/movie.routes.js
@@ -17,8 +17,9 @@ const upload = multer({ storage: storage });
 
 router.post(
   "/",
+  secureAPI(["admin"]),
 
-  upload.array("images",4),
+  upload.array("images", 4),
   async (req, res, next) => {
     try {
       if (req.files) {
@@ -36,11 +37,63 @@ router.post(
       req.body.created_by = req.currentUser;
 
       const result = await controller.create(req.body);
-      res.json({ data:result,  msg: "success" });
+      res.json({ data: result, msg: "success" });
     } catch (e) {
       next(e);
     }
   }
 );
+
+router.get("/", async (req, res, next) => {
+  try {
+    const { limit, page, name, isArchive } = req.query;
+    const search = { name, isArchive };
+    const result = await controller.list(limit, page, search);
+    res.json({ data: result, msg: "success" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const result = await controller.getById(req.params.id);
+    res.json({ data: result, msg: "success" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.put(
+  "/:id",
+  secureAPI(["admin"]),
+  upload.array("images", 4),
+  async (req, res, next) => {
+    try {
+      if (req.files) {
+        req.body.images = [];
+        req.files.map((file) =>
+          req.body.images.push("products/".concat(file.filename))
+        );
+      }
+
+      req.body.updated_by = req.currentUser;
+      const result = await controller.updateById(req.params.id, req.body);
+      res.json({ data: result, msg: "success" });
+    } catch (e) {
+      next(e);
+    }
+  }
+);
+
+router.delete("/:id", secureAPI(["admin"]), async (req, res, next) => {
+  try {
+    req.body.updated_by = req.currentUser;
+    const result = await controller.deleteById(req.params.id, req.body);
+    res.json({ data: result, msg: "success" });
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = router;
